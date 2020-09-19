0617978fd87efd4c13373957eb2a4698b7077660
VassileiosXristopoulos <vpxristop@gmail.com>
2020-09-19 14:45:12 +0300

    added table for user requests -  cleaned up code

--- user_dashboard.php
+++ user_dashboard.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require('connect.php');
+require('functions.php');
 
 
 $user_id = $_SESSION['user_id'];
@@ -9,9 +10,28 @@ $result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
 
 $vacations = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-print_r($vacations);
+//TODO: make table scrollable
 ?>
+ <table style="width:50%">
+    <tr>
+        <th>Date Submitted</th>
+        <th>Date From</th>
+        <th>Date To</th>
+        <th>Days requested</th>
+        <th>Status</th>
+    </tr>
+    <?php foreach($vacations as $vacation){ ?>
 
+        <tr>
+        <th><?php echo $vacation['date_submitted']; ?></th>
+        <th><?php echo $vacation['date_from']; ?></th>
+        <th><?php echo $vacation['date_to']; ?></th>
+        <th><?php echo getDatesDifference($vacation['date_from'], $vacation['date_to'])?></th>
+        <th><?php echo $vacation['status']; ?></th>
+    </tr>
+
+    <?php } ?>
+</table>
 <a href="/epignosis_portal/create_vacation.php" > Create new vacation </a>
 
 <?php
