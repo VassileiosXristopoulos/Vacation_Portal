2f2b46d3f5dd19f140a4997dc8ff0e295d23b2ac
VassileiosXristopoulos <vpxristop@gmail.com>
2020-09-19 14:45:55 +0300

    minor fixes to information passed by form submission

--- login.php
+++ login.php
@@ -5,24 +5,26 @@ require('connect.php');
 //3.1 If the form is submitted
 if (isset($_POST['username']) and isset($_POST['password'])){
     //3.1.1 Assigning posted values to variables.
-    $username = $_POST['username'];
+    $email = $_POST['username'];
     $password = $_POST['password'];
     //3.1.2 Checking the values are existing in the database or not
-    $query = "SELECT * FROM `users` WHERE email='$username' and user_pass='$password'";
+    $query = "SELECT * FROM `users` WHERE email='$email' and user_pass='$password'";
 
     $result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
     $count = mysqli_num_rows($result);
     //3.1.2 If the posted values are equal to the database values, then session will be created for the user.
     if ($count == 1){
-        
-        $_SESSION['user_id'] = $result->fetch_object()->id;
-        $_SESSION['username'] = $username;
+        $user = $result->fetch_object();
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['email'] = $email;
+        $_SESSION['fullname'] = $user->firstname . " ". $user->lastname;
+
         
         if($result->fetch_object()->is_admin){
-            header("Location:admin_dashboard.php"); //header to redirect, but doesnt work
+            header("Location:admin_dashboard.php"); //header to redirect
         }
         else{
-            header("Location:user_dashboard.php"); //header to redirect, but doesnt work
+            header("Location:user_dashboard.php"); //header to redirect
         }
 
         die;
