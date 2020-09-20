b3d7b5ed321ece0513168ad1251117754549b2c7
VassileiosXristopoulos <vpxristop@gmail.com>
2020-09-20 15:58:50 +0300

    Added links to redirect to user's page. Will be changed with javascript.

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,17 +1,17 @@
 <?php 
 session_start();
+require('header.php');
 require('connect.php');
 require('functions.php');
 
-
-$user_id = $_SESSION['user_id'];
 $query = "SELECT * FROM `users`";
 $result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
 
 $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
 ?>
-
-
+<a href="/epignosis_portal/user.php?user=new_user" > Create new user </a>
 
 <table style="width:50%">
 <tr>
@@ -20,14 +20,36 @@ $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <th>User email</th>
     <th>User type</th>
 </tr>
-<?php foreach($users as $user){ ?>
 
+<?php //TODO: Make row clickable and redirect via javascript! ?>
+<?php foreach($users as $user){
+    $user_page = "/epignosis_portal/user.php?user=". $user['id'];
+    ?>
+    
     <tr>
-    <th><?php echo $user['firstname']; ?></th>
-    <th><?php echo $user['lastname']; ?></th>
-    <th><?php echo $user['email']; ?></th>
-    <th><?php echo $user['is_admin'] == 1 ? "admin" : "employee"; ?></th>
-</tr>
+        <th>
+            <a href="<?php echo $user_page ?>">
+                <?php echo $user['firstname']; ?>
+            </a>
+        </th>
+        <th>
+            <a href="<?php echo $user_page ?>">
+                <?php echo $user['lastname']; ?>
+            </a>
+        </th>
+        <th>
+            <a href="<?php echo $user_page ?>">
+                <?php echo $user['email']; ?>
+            </a>
+        </th>
+        <a href="<?php echo $user_page ?>">
+            <th><?php echo $user['is_admin'] == 1 ? "admin" : "employee"; ?></th>
+        </a>
+    </tr>
+    
 
 <?php } ?>
 </table>
+
+<?php
+require('footer.php');
