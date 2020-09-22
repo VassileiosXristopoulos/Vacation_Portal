67d8b6d907ff7c138b6e0c2a179920c1fcef325f
VassileiosXristopoulos <vpxristop@gmail.com>
2020-09-22 18:23:05 +0300

    Added checks for only logged in user and employee accessing page Minor changes to table structure

--- user_dashboard.php
+++ user_dashboard.php
@@ -1,9 +1,22 @@
 <?php
 session_start();
+
+if(!isset($_SESSION['user_id'])) {
+    header("Location: /epignosis_portal/index.php");
+    exit();
+}
+
 require('header.php');
 require('connect.php');
 require('functions.php');
 
+
+if($_SESSION['is_admin']){
+    echo "Cannot acces page. The Create Vacation page is for Employees";
+    echo '</br> <a href="/epignosis_portal/admin_dashboard.php">Go to admin dashboard </a>';
+    require('footer.php');
+    die;
+}
 
 $user_id = $_SESSION['user_id'];
 $query = "SELECT * FROM `vacations` WHERE user_id='$user_id'";
@@ -13,7 +26,8 @@ $vacations = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 //TODO: make table scrollable
 ?>
- <table style="width:50%">
+ <table class="portal-table vacations-table">
+ <thead>
     <tr>
         <th>Date Submitted</th>
         <th>Date From</th>
@@ -21,6 +35,8 @@ $vacations = mysqli_fetch_all($result, MYSQLI_ASSOC);
         <th>Days requested</th>
         <th>Status</th>
     </tr>
+<thead>
+<tbody>
     <?php foreach($vacations as $vacation){ ?>
 
         <tr>
@@ -32,9 +48,10 @@ $vacations = mysqli_fetch_all($result, MYSQLI_ASSOC);
     </tr>
 
     <?php } ?>
+</tbody>
 </table>
 <?php // TODO: check for better way of writting path ?>
-<a href="/epignosis_portal/create_vacation.php" > Create new vacation </a>
+<a id="new_vacation" href="/epignosis_portal/create_vacation.php" > Create new vacation </a>
 
 <?php
 mysqli_free_result($result);
