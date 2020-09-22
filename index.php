b8020c4ef1b902f118373f96120467617d67ed47
VassileiosXristopoulos <vpxristop@gmail.com>
2020-09-22 18:19:50 +0300

    Minor change to title placement

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php 
 require('header.php');
 require('login.php');
-
-// TODO: lock access to portal's pages unless user is logged in
 ?>
 <!-- Form for logging in the users -->
 
 <div class="register-form">
 
-    <p>Please login to continue</p>
-
     <h1>Login</h1>
+    <p>Please login to continue</p>
     <form action="" method="POST">
         <p><label>User Name : </label>
         <input id="username" type="text" name="username" required placeholder="username" /></p>
